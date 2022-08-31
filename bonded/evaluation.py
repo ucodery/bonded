b2958b7648f915b89f167b404ceaf0a44b97880d
Jeremy Paige <ucodery@gmail.com>
2022-08-30 23:37:40 -0700

    refactor evaluation

--- bonded/evaluation.py
+++ bonded/evaluation.py
@@ -8,7 +8,7 @@ from ._sys import stdlib_module_names
 from .executable_inspection import ExecutableInspection
 
 from .module_inspection import Module, ModuleInspection
-from .package_inspection import PackageInspection
+from .package_inspection import Package, PackageInspection
 
 
 @dataclasses.dataclass
@@ -25,10 +25,9 @@ class Report:
 
 @dataclasses.dataclass
 class ModuleScore:
+    module: Module
     belongs_to_package: Optional[str] = None
     matches_package: Optional[str] = None
-    imported_with_statement: bool = False
-    imported_with_function: bool = False
     stdlib: bool = False
     local_project: bool = False
     user_ignored: bool = False
@@ -36,11 +35,11 @@ class ModuleScore:
     def passes(self):
         return (
             (self.belongs_to_package is not None or self.matches_package is not None)
-            and (self.imported_with_statement or self.imported_with_function)
+            and (self.module.found_import_stmt or self.module.found_import_fun)
         ) or (self.stdlib or self.local_project or self.user_ignored)
 
 
-class ModuleEvaluation:
+class ModuleEvaluation(dict):
     def __init__(self, modules, ignore_modules=None, project_modules=None, stdlib_modules=None):
         ignore_modules = ignore_modules or []
         project_modules = project_modules or []
@@ -48,8 +47,7 @@ class ModuleEvaluation:
         self.modules = modules
         self.module_scores = {
             mod_name: ModuleScore(
-                imported_with_statement=mod.found_import_stmt,
-                imported_with_function=mod.found_import_fun,
+                mod,
                 stdlib=mod_name in stdlib_modules,
                 local_project=mod_name in project_modules,
                 user_ignored=mod_name in ignore_modules,
@@ -58,83 +56,84 @@ class ModuleEvaluation:
         }
 
     def evaluate_with_packages(self, packages):
-        for mod in self.modules.values():
+        for mscore in self.values():
             for pkg in packages.values():
-                if mod.name in pkg.modules:
-                    self.module_scores[mod.name].belongs_to_package = pkg.name
+                if mscore.module.name in pkg.modules:
+                    mscore.belongs_to_package = pkg.name
                     break
-                if mod.name == pkg.name:
-                    self.module_scores[mod.name].matches_package = pkg.name
+                if mscore.module.name == pkg.name:
+                    mscore.matches_package = pkg.name
                     break
 
     def report(self):
-        return {
-            self.modules[mod] for mod, score in self.module_scores.items() if not score.passes()
-        }
+        return {mscore.module for mscore in self.values() if not mscore.passes()}
 
 
-# NOTE: just keep using Package?
 @dataclasses.dataclass
 class PackageScore:
-    installed: bool = False
-    platform_ignored: bool = False
+    package: Package
     user_ignored: bool = False
     module_imported: bool = False
     extension_used: bool = False
     executable_called: bool = False
 
+    @property
+    def platform_ignored(self):
+        return self.package.markers and not any(mark.evaluate() for mark in self.package.markers)
+
     def passes(self):
         return (
-            self.installed
+            self.package.installed
             and (self.module_imported or self.extension_used or self.executable_called)
         ) or (self.platform_ignored or self.user_ignored)
 
 
-class PackgeEvaluation:
+class PackgeEvaluation(dict):
     def __init__(self, packages, ignore_packages=None):
         ignore_packages = ignore_packages or []
-        self.packages = packages
-        self.package_scores = {
-            pkg_name: PackageScore(
-                installed=pkg.installed,
-                platform_ignored=(pkg.markers and not any(mark.evaluate() for mark in pkg.markers)),
-                user_ignored=pkg_name in ignore_packages,
+        super().__init__(
+            (
+                pkg_name,
+                PackageScore(
+                    package=pkg,
+                    user_ignored=pkg_name in ignore_packages,
+                ),
             )
-            for pkg_name, pkg in self.packages.items()
-        }
+            for pkg_name, pkg in packages.items()
+        )
 
     def evaluate_with_modules(self, modules):
-        for pkg in self.packages.values():
-            for mod in pkg.modules:
+        for pscore in self.values():
+            for mod in pscore.package.modules:
                 # TODO: should be based on ModuleEvaluation
                 if mod in modules and (
                     modules[mod].found_import_stmt or modules[mod].found_import_fun
                 ):
-                    self.package_scores[pkg.name].module_imported = True
+                    self[pscore.package.name].module_imported = True
                     break
 
-    def evaluate_with_extensions(self, modules, packages):
-        passing = sum(score.passes() for score in self.package_scores.values()) + 1
-        while passing > sum(score.passes() for score in self.package_scores.values()):
-            for pkg in self.packages.values():
-                if self.package_scores[pkg.name].extension_used:
+    def evaluate_with_extensions(self, modules):
+        passing = sum(score.passes() for score in self.values()) + 1
+        while passing > sum(score.passes() for score in self.values()):
+            for pscore in self.values():
+                if self[pscore.package.name].extension_used:
                     continue
-                for ext in pkg.extends:
+                for ext in pscore.package.extends:
                     # TODO: should be based on ModuleEvaluation
                     if (
                         ext in modules
                         and (modules[ext].found_import_stmt or modules[ext].found_import_fun)
-                        or (ext in packages)
+                        or (ext in self)
                     ):
-                        self.package_scores[pkg.name].extension_used = True
+                        self[pscore.package.name].extension_used = True
                         break
-            passing = sum(score.passes() for score in self.package_scores.values())
+            passing = sum(score.passes() for score in self.values())
 
     def evaluate_with_executables(self, executables):
-        for pkg in self.packages.values():
-            for exe in pkg.executables:
+        for pscore in self.values():
+            for exe in pscore.package.executables:
                 if exe in executables and executables[exe].found_executions:
-                    self.package_scores[pkg.name].executable_called = True
+                    self[pscore.package.name].executable_called = True
                     break
 
     def evaluate_build_backend(self, pyproject):
@@ -147,9 +146,9 @@ class PackgeEvaluation:
                 return
 
             backend = Module(build_backend.split(':')[0].split('.')[0])
-            for pkg in self.packages.values():
-                if backend.name in pkg.modules:
-                    self.package_scores[pkg.name].module_imported = True
+            for pscore in self.values():
+                if backend.name in pscore.package.modules:
+                    self[pscore.package.name].module_imported = True
                     # if backend_path: ModuleEvaluation.local_project = True
                     break
 
@@ -158,26 +157,24 @@ class PackgeEvaluation:
 
         ANY of these that can be removed from here and worked into more general logic should be
         """
-        for pkg in self.packages.values():
-            if pkg.name == 'wheel':
+        for pscore in self.values():
+            if pscore.package.name == 'wheel':
                 # wheel only extends distutils, but setuptools is more frequently imported
                 # TODO check that setuptools is executed, not imported
                 if 'setuptools' in modules and (
                     modules['setuptools'].found_import_stmt
                     or modules['setuptools'].found_import_fun
                 ):
-                    self.package_scores[pkg.name].extension_used = True
+                    self[pscore.package.name].extension_used = True
             elif (
-                'pytest11' in pkg.extends
-                and 'pytest' in self.package_scores
-                and self.package_scores['pytest'].passes()
+                'pytest11' in pscore.package.extends
+                and 'pytest' in self
+                and self['pytest'].passes()
             ):
-                self.package_scores[pkg.name].extension_used = True
+                self[pscore.package.name].extension_used = True
 
     def report(self):
-        return {
-            self.packages[pkg] for pkg, score in self.package_scores.items() if not score.passes()
-        }
+        return {pscore.package for pscore in self.values() if not pscore.passes()}
 
 
 def evaluate_bonds(settings, modules, packages, executables):
@@ -189,7 +186,7 @@ def evaluate_bonds(settings, modules, packages, executables):
     excess_packages = PackgeEvaluation(packages, settings.ignore_packages)
     excess_packages.evaluate_with_modules(modules)
     excess_packages.evaluate_with_executables(executables)
-    excess_packages.evaluate_with_extensions(modules, packages)
+    excess_packages.evaluate_with_extensions(modules)
     if settings.pyproject:
         excess_packages.evaluate_build_backend(settings.pyproject)
     excess_packages.apply_passes(modules)
